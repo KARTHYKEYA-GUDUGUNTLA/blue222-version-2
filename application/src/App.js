0a94c2afc8b6453708a71afea5c42d4a2c77b7d7
Guduguntla Karthykeya <i-kguduguntla@vsoftconsulting.com>
2024-03-20 17:26:44 +0530

    updated_20TH_march

--- application/src/App.js
+++ application/src/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Example from './SignUp';
+import SignUp from './SignUp';
 
 function App() {
   return (
    
       <div className="App">
-      <Example />
+      <SignUp />
       </div>
   );
 }
